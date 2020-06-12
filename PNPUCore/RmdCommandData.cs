d60386221504bc6df8b78578880f1efe360a72eb
tbourgois <tbourgois@cegid.com>
2020-06-12 15:23:29 +0200

    RmdCommandData

---
+++ PNPUCore/RmdCommandData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNPUCore.Process
+{
+    class RmdCommandData
+    {
+
+        String idPackage;
+        String idClass;
+        String cmdCode;
+
+        public RmdCommandData(string idPackage, string idClass, string cmdCode)
+        {
+            this.idPackage = idPackage;
+            this.idClass = idClass;
+            this.CmdCode = cmdCode;
+        }
+
+        public string IdPackage { get => idPackage; set => idPackage = value; }
+        public string IdClass { get => idClass; set => idClass = value; }
+        public string CmdCode { get => cmdCode; set => cmdCode = value; }
+    }
+}
+
