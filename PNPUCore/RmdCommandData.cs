b1315a5cec0dcd6f33a60390d29e2df0c2f4be77
tbourgois <tbourgois@cegid.com>
2020-06-12 15:51:51 +0200

    RmdCommandData

--- PNPUCore/RmdCommandData.cs
+++ PNPUCore/RmdCommandData.cs
@@ -12,17 +12,20 @@ namespace PNPUCore.Process
         String idPackage;
         String idClass;
         String cmdCode;
+        IProcess process;
 
-        public RmdCommandData(string idPackage, string idClass, string cmdCode)
+        public RmdCommandData(string idPackage, string idClass, string cmdCode, IProcess process)
         {
             this.idPackage = idPackage;
             this.idClass = idClass;
-            this.CmdCode = cmdCode;
+            this.cmdCode = cmdCode;
+            this.process = process;
         }
 
         public string IdPackage { get => idPackage; set => idPackage = value; }
         public string IdClass { get => idClass; set => idClass = value; }
         public string CmdCode { get => cmdCode; set => cmdCode = value; }
+        public IProcess Process { get => process; set => process = value; }
     }
 }
 
