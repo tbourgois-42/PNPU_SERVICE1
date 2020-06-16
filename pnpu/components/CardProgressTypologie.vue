9856b2e16b47c027c7237bdbe54ef94c0fb0e5f3
Maxime Duquenne <mduquenne@cegid.com>
2020-06-16 09:07:26 +0200

    Evol - Voir uniquement la progression des clients du dashboard

--- pnpu/components/CardProgressTypologie.vue
+++ pnpu/components/CardProgressTypologie.vue
@@ -10,78 +10,84 @@
       </v-card-title>
       <v-divider class="mx-4"></v-divider>
       <v-card-text>
-        <div class="mb-4 subtitle">
-          Saas Dédié<v-icon
-            v-if="progressSaaSDedie === 100"
-            class="mx-4"
-            color="success"
-            small
-            >mdi-check-decagram</v-icon
-          >
+        <div v-if="showProgressBarSaasDedie === true">
+          <div class="mb-4 subtitle">
+            Saas Dédié<v-icon
+              v-if="progressSaaSDedie === 100"
+              class="mx-4"
+              color="success"
+              small
+              >mdi-check-decagram</v-icon
+            >
+          </div>
+          <div>
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-progress-linear
+                  height="10"
+                  :value="progressSaaSDedie"
+                  striped
+                  class="my-4"
+                  :color="colorCircularSaaSDedie"
+                  v-on="on"
+                ></v-progress-linear>
+              </template>
+              <span>{{ progressSaaSDedie }}</span>
+            </v-tooltip>
+          </div>
         </div>
-        <div>
-          <v-tooltip top>
-            <template v-slot:activator="{ on }">
-              <v-progress-linear
-                height="10"
-                :value="progressSaaSDedie"
-                striped
-                class="my-4"
-                :color="colorCircularSaaSDedie"
-                v-on="on"
-              ></v-progress-linear>
-            </template>
-            <span>{{ progressSaaSDedie }}</span>
-          </v-tooltip>
+        <div v-if="showProgressBarSaasDesynchronise === true">
+          <div class="mb-4 subtitle">
+            Saas Désynchronisé<v-icon
+              v-if="progressSaaSDesynchronise === 100"
+              class="mx-4"
+              color="success"
+              small
+              >mdi-check-decagram</v-icon
+            >
+          </div>
+          <div>
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-progress-linear
+                  height="10"
+                  :value="progressSaaSDesynchronise"
+                  striped
+                  class="my-4"
+                  :color="colorCircularSaaSDesync"
+                  v-on="on"
+                ></v-progress-linear>
+              </template>
+              <span>{{ progressSaaSDesynchronise }}</span>
+            </v-tooltip>
+          </div>
         </div>
-        <div class="mb-4 subtitle">
-          Saas Désynchronisé<v-icon
-            v-if="progressSaaSDesynchronise === 100"
-            class="mx-4"
-            color="success"
-            small
-            >mdi-check-decagram</v-icon
-          >
-        </div>
-        <div>
-          <v-tooltip top>
-            <template v-slot:activator="{ on }">
-              <v-progress-linear
-                height="10"
-                :value="progressSaaSDesynchronise"
-                striped
-                class="my-4"
-                :color="colorCircularSaaSDesync"
-                v-on="on"
-              ></v-progress-linear>
-            </template>
-            <span>{{ progressSaaSDesynchronise }}</span>
-          </v-tooltip>
-        </div>
-        <div class="mb-4 subtitle">
-          SaaS Mutualisé<v-icon
-            v-if="progressPlateforme === 100"
-            class="mx-4"
-            color="success"
-            small
-            >mdi-check-decagram</v-icon
-          >
-        </div>
+        <div v-if="showProgressBarSaasMutualise === true">
+          <div class="mb-4 subtitle">
+            SaaS Mutualisé<v-icon
+              v-if="progressPlateforme === 100"
+              class="mx-4"
+              color="success"
+              small
+              >mdi-check-decagram</v-icon
+            >
+          </div>
 
-        <div>
-          <v-tooltip top>
-            <template v-slot:activator="{ on }">
-              <v-progress-linear
-                height="10"
-                :value="progressPlateforme"
-                striped
-                class="mt-4"
-                :color="colorCircularSaaSPlat"
-                v-on="on"
-              ></v-progress-linear>
-            </template>
-            <span>{{ progressPlateforme }}</span>
-          </v-tooltip>
+          <div>
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-progress-linear
+                  height="10"
+                  :value="progressPlateforme"
+                  striped
+                  class="mt-4"
+                  :color="colorCircularSaaSPlat"
+                  v-on="on"
+                ></v-progress-linear>
+              </template>
+              <span>{{ progressPlateforme }}</span>
+            </v-tooltip>
+          </div>
         </div>
       </v-card-text>
     </v-card>
@@ -106,7 +112,10 @@ export default {
       colorCircularSaaSDedie: 'teal lighten-2',
       colorCircularSaaSDesync: 'lime lighten-2',
       localClients: '',
-      iconFilter: 'mdi-filter'
+      iconFilter: 'mdi-filter',
+      showProgressBarSaasDedie: false,
+      showProgressBarSaasDesynchronise: false,
+      showProgressBarSaasMutualise: false
     }
   },
 
@@ -125,14 +134,20 @@ export default {
       let nbClientDesync = 0
       let nbClientPlat = 0
       let nbClientDedie = 0
+      this.showProgressBarSaasDesynchronise = false
+      this.showProgressBarSaasDedie = false
+      this.showProgressBarSaasMutualise = false
       this.localClients.forEach((element) => {
         if (element.TYPOLOGY === 'SAAS DESYNCHRONISE') {
+          this.showProgressBarSaasDesynchronise = true
           progressSDesync = progressSDesync + element.PERCENTAGE_COMPLETUDE
           nbClientDesync = nbClientDesync + 1
         } else if (element.TYPOLOGY === 'SAAS DEDIE') {
+          this.showProgressBarSaasDedie = true
           progressSDedie = progressSDedie + element.PERCENTAGE_COMPLETUDE
           nbClientDedie = nbClientDedie + 1
         } else if (element.TYPOLOGY === 'SAAS MUTUALISE') {
+          this.showProgressBarSaasMutualise = true
           progressPlat = progressPlat + element.PERCENTAGE_COMPLETUDE
           nbClientPlat = nbClientPlat + 1
         }
